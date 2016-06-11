4cb72043aa2359115fe2347333961e519b0d6f11
Chiel ten Brinke <chiel.tenbrinke@intellimagic.com>
2016-06-11 23:17:10 +0200

    Enable pypi uploads.

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-  name = 'shellout',
-  packages = ['shellout'], # this must be the same as the name above
-  version = '0.1',
-  description = 'Thin convenience wrappers for shelling out commands easily from python',
-  author = 'Chiel ten Brinke',
-  author_email = 'ctenbrinke@gmail.com',
-  url = 'https://github.com/Chiel92/python-shellout', # use the URL to the github repo
-  download_url = 'https://github.com/Chiel92/python-shellout/tarball/0.1', # I'll explain this in a second
-  keywords = ['shellout'], # arbitrary keywords
-  classifiers = [],
+    name='shellout',
+    packages=['shellout'],  # this must be the same as the name above
+    version='0.1.1',
+    description='Thin convenience wrappers for shelling out commands easily from python',
+    author='Chiel ten Brinke',
+    author_email='ctenbrinke@gmail.com',
+    url='https://github.com/Chiel92/python-shellout',  # use the URL to the github repo
+    # download_url='https://github.com/Chiel92/python-shellout/tarball/0.1',  # Github tarball download
+    keywords=['shellout'],  # arbitrary keywords
+    classifiers=[],
 )
