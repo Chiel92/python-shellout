9f5fdb1ed497e760a93cd9738f7e08592b490994
Chiel ten Brinke <chiel.tenbrinke@intellimagic.com>
2016-06-15 17:22:34 +0200

    Increase version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     name='pyshellout',
     packages=['pyshellout'],  # this must be the same as the name above
-    version='0.1.2',
+    version='0.2',
     description='Thin convenience wrappers for shelling out commands easily from python',
     author='Chiel ten Brinke',
     author_email='ctenbrinke@gmail.com',
